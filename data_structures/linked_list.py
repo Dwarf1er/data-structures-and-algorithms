d4395a3bf0d650560ebf932c4fe1c34b0dd53931
Dwarf1er <SerolTech@protonmail.com>
2023-08-15 20:57:42 -0400

    Added pop method to LinkedList and unit tests.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -24,6 +24,23 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+
+    def pop(self) -> Node:
+        if self.length == 0:
+            return None
+        else:
+            pointer: Node = self.head
+            removed_node: Node = self.tail
+            while pointer.next is not self.tail and pointer.next is not None:
+                pointer = pointer.next
+            self.tail = pointer
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return removed_node
+
+        
     
     def print_linked_list(self) -> None:
         pointer: Node = self.head
